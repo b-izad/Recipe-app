909186354f851ff981fbe1862d2d12e644a3f552
Bahareh <izad@bu.edu>
2023-04-06 14:42:44 -0400

    Added the basics to app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "react";
 
 function App() {
+const APP_ID= 'b6c0ce0b'
+const APP_KEY= 'b55444e88ae3adfaafc67fb9bfc6549f'
+const example_req = 'https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata'
+
+const [recipe, setRecipe] = useState([]);
+useEffect(()=>{
+  getR()
+} , []);
+
+const getR = async () => {
+  try{
+  const res = await fetch (example_req)
+  const data = await res.json()
+  setRecipe(data.meals)}
+  catch(error){console.log(error)}
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     Hello world
+    <form className="search">
+      <input className="search-bar" type="text"/>
+      <button className="search-button" type="submit">
+        
+        </button>
+
+      
+    </form>
     </div>
   );
 }
